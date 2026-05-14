9cfbc0c3f2bf3a52220050018b4e13bd133a9e1c
Andriy007i <andrii.zub@nure.ua>
2026-05-14 09:03:45 +0300

    Delete src directory

--- src/api.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
-
-export const fetchTasks = () => API.get('/tasks');
-export const createTask = (newTask) => API.post('/tasks', newTask);
